da6ac78976beeb39460ff106f4c44362f9e00871
Mark Samoylov <mark.saioilov@yandex.ru>
2025-12-24 22:22:37 +0300

    Task_08.sol

--- tasks/Task_08.sol
+++ tasks/Task_08.sol
@@ -16,4 +16,11 @@ contract Task_08 {
    function getDynamicByteArrayLength(bytes memory _data) public pure returns (uint256) {
        return _data.length;
    }
+
+   function getDynamicByteArrayElement(bytes memory _data, uint256 _index) 
+        public pure returns (bytes1) 
+    {
+        require(_index < _data.length, "Index out of bounds");
+        return _data[_index];
+    }
 }
